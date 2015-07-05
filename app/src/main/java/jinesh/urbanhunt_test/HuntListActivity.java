8e2e1c2dc07f41057c91eed5cdef5f0a43d3687c
Jinesh510 <jineshshah510@gmail.com>
2015-07-05 12:14:19 +0530

    Updated huntListAdapter Class

--- app/src/main/java/jinesh/urbanhunt_test/HuntListActivity.java
+++ app/src/main/java/jinesh/urbanhunt_test/HuntListActivity.java
@@ -2,13 +2,22 @@ package jinesh.urbanhunt_test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jinesh on 03/07/15.
  */
 public class HuntListActivity extends Activity {
 
-    private Hunt hunt;
+    private HuntListAdapter adapterHunts;
+    private ListView lvHunts;
 
     public HuntListActivity() {
         super();
@@ -17,10 +26,31 @@ public class HuntListActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        hunt = new Hunt();
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.hunt_list);
+        lvHunts = (ListView)findViewById(R.id.lvHunts);
+        ArrayList<Hunt> aHunts = new ArrayList<Hunt>();
+        adapterHunts = new HuntListAdapter(this,aHunts);
+        lvHunts.setAdapter(adapterHunts);
 
+        fetchHunts();
 
     }
+
+    private void fetchHunts() {
+
+        ParseQuery<Hunt> huntParseQuery = ParseQuery.getQuery(Hunt.class);
+        huntParseQuery.findInBackground(new FindCallback<Hunt>() {
+            @Override
+            public void done(List<Hunt> list, ParseException e) {
+                for (Hunt h : list){
+
+                    adapterHunts.add(h);
+                }
+                adapterHunts.notifyDataSetChanged();
+            }
+        });
+    }
+
+
 }
